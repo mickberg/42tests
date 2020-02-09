7355f35eb681ba942ba1dd61220a14926f9dffd0
mickberg <mikael.berglund2@gmail.com>
2020-02-09 20:12:19 +0200

    remove false tests

--- ft_printf/srcs/tests/test_basics.c
+++ ft_printf/srcs/tests/test_basics.c
@@ -6,7 +6,7 @@
 /*   By: mikaelberglund <marvin@42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/07 16:40:56 by mikaelber         #+#    #+#             */
-/*   Updated: 2020/02/09 19:37:59 by mikaelber        ###   ########.fr       */
+/*   Updated: 2020/02/09 20:12:01 by mikaelber        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,12 @@ void	test_basics(t_testinfo *info)
 	run_test(info, "empty format text no arg", "");
 	run_test(info, "escaped chars", "\t\r\n\v\f\\");
 
-	// should fail
-	run_test(info, "should fail", "%e", 10);
-
 	// %
 	run_test(info, "precentage sign", "%");
 	run_test(info, "precentage sign width", "5%");
 	run_test(info, "precentage sign width 0flag", "05%");
 	run_test(info, "precentage sign width 0flag lalgin", "-05%");
 	run_test(info, "precentage sign width lalign", "-5%");
-
-	// should fail
-	run_test(info, "should fail", "%.2e", 10.133);
 
 	// no specifiers
 	run_test(info, "no spec", "%");
